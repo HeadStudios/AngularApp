78897009610d6ccee5a9941da8f83e631f242be7
Sakhamuzi <9292544+Sakhamen@users.noreply.github.com>
2024-03-04 13:37:33 +0200

    Add new task submit fix

--- src/app/add-task-modal/add-task-modal.component.ts
+++ src/app/add-task-modal/add-task-modal.component.ts
@@ -1,19 +1,19 @@
-import { Component, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http';
-import { ToastController } from '@ionic/angular';
+import { Component, Input } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { HttpClient } from "@angular/common/http";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-add-task-modal',
-  templateUrl: './add-task-modal.component.html',
-  styleUrls: ['./add-task-modal.component.scss'],
+  selector: "app-add-task-modal",
+  templateUrl: "./add-task-modal.component.html",
+  styleUrls: ["./add-task-modal.component.scss"],
 })
 export class AddTaskModalComponent {
   @Input() injusticeId: number;
 
-  title: string = '';
-  description: string = '';
-  dueDate: string = '';
+  title: string = "";
+  description: string = "";
+  dueDate: string = "";
 
   constructor(
     private modalController: ModalController,
@@ -24,7 +24,7 @@ export class AddTaskModalComponent {
   async presentToast(message: string) {
     const toast = await this.toastController.create({
       message: message,
-      duration: 2000
+      duration: 2000,
     });
     toast.present();
   }
@@ -36,21 +36,22 @@ export class AddTaskModalComponent {
       due_date: dueDate,
       injustice_id: this.injusticeId,
     };
-  
+
+    console.log("submitTask taskData", taskData);
+
     //const url = `https://rrdevours.monster/api/injustices/${injusticeId}/tasks`;
     const url = `https://webhook.site/ec91efbc-0a4a-4665-9b2d-3bf26fa73693`;
-    // 
-  
+
     this.http.post(url, taskData).subscribe({
       next: (response) => {
-        console.log('Task added successfully', response);
-        this.presentToast('Task added successfully');
+        console.log("Task added successfully", response);
+        this.presentToast("Task added successfully");
         this.modalController.dismiss({ taskAdded: true });
       },
       error: (error) => {
-        console.error('Error adding task', error);
-        this.presentToast('Error adding task');
-      }
+        console.error("Error adding task", error);
+        this.presentToast("Error adding task");
+      },
     });
   }
 
