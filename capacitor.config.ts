0aa9cc1251086c47bc7d9072ffcd6f20c6c2e0c4
Sakhamuzi <9292544+Sakhamen@users.noreply.github.com>
2024-03-10 19:31:07 +0200

    Feature: Capacitor Push Notifications.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,24 +1,29 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'monster.rrdevours.app',
-  appName: 'Sunlight Quest',
-  webDir: 'www',
+  appId: "monster.rrdevours.app",
+  appName: "Sunlight Quest",
+  webDir: "www",
   server: {
-    androidScheme: 'https'
+    androidScheme: "https",
+  },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
+    },
   },
   cordova: {
     preferences: {
-      ScrollEnabled: 'false',
-      'android-minSdkVersion': '19',
-      BackupWebStorage: 'none',
-      SplashMaintainAspectRatio: 'true',
-      FadeSplashScreenDuration: '300',
-      SplashShowOnlyFirstTime: 'false',
-      SplashScreen: 'screen',
-      SplashScreenDelay: '3000'
-    }
-  }
+      ScrollEnabled: "false",
+      "android-minSdkVersion": "19",
+      BackupWebStorage: "none",
+      SplashMaintainAspectRatio: "true",
+      FadeSplashScreenDuration: "300",
+      SplashShowOnlyFirstTime: "false",
+      SplashScreen: "screen",
+      SplashScreenDelay: "3000",
+    },
+  },
 };
 
 export default config;
